21723e247d1698f085aeb7d758efda2e7cc7af82
anggllpz11 <Desarrollador@memcosas.com>
2025-01-21 13:04:42 -0500

    Header 90%

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -4,56 +4,60 @@ import logo from "../images/head/logoBioswift.png";
 import '../styles/head.css'
 
 export const Head = (): JSX.Element => {
+  const handleLoginClick = () => {
+    // Redirección externa
+    window.open("https://bioswift.net", "_blank");
+  };
+
+  const handleMemcoClick = () => {
+    // Redirección externa
+    window.open("https://memcosas.com", "_blank");
+  };
+
   return (
     <div className="head">
       <header className="header">
         <div className="overlap">
           <div className="side-effects">
             <div className="overlap-group">
-              <div className="rectangle" />
-
-              <div className="div" />
+              <div className="rectangle"></div>
+              <div className="div"></div>
             </div>
           </div>
 
           <div className="nav">
-            <div className="login">
+            <div className="login" onClick={handleLoginClick}>
               <div className="overlap-group-2">
-                <div className="rectangle-2" />
-
+                <div className="rectangle-2"></div>
                 <div className="text-wrapper">LOGIN</div>
               </div>
             </div>
 
-            <div className="memco">
+            <div className="memco" onClick={handleMemcoClick}>
               <div className="overlap-group-2">
-                <div className="rectangle-3" />
-
-                <div className="text-wrapper-2">MEMCO</div>
+                <div className="rectangle-3">MEMCO</div>
+                MEMCO
               </div>
             </div>
 
             <div className="location">
               <div className="overlap-group-2">
-                <div className="rectangle-3" />
-
-                <div className="text-wrapper-3">LOCATION</div>
+                <div className="rectangle-3">LOCATION</div>
+                LOCATION
               </div>
             </div>
 
             <div className="about">
               <div className="overlap-group-2">
-                <div className="rectangle-3" />
-
-                <div className="text-wrapper-4">ABOUT US</div>
+                <div className="rectangle-3">ABOUT US</div>
+                ABOUT US
               </div>
             </div>
 
             <div className="contact">
               <div className="overlap-group-2">
-                <div className="rectangle-3" />
-
-                <div className="text-wrapper-5">CONTACTO</div>
+                <div className="rectangle-3">CONTACTO</div>
+                CONTACTO
               </div>
             </div>
 
@@ -62,7 +66,6 @@ export const Head = (): JSX.Element => {
 
           <div className="bioinfo">
             <img className="logo" alt="Logo" src={logo} />
-
             <div className="bioswift">BIOSWIFT</div>
           </div>
         </div>
